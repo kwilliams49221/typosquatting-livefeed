4329c1e39baeb4aeb5091bd0d8503731e6bf8437
Kaleb Williams <kw@kalebwilliams.com>
2024-04-13 14:24:29 -0400

    Add chmod to setup for startfile

--- setup.sh
+++ setup.sh
@@ -4,3 +4,4 @@ pip install -r ./python-backend/requirements.txt
 pip install docker-compose
 docker build -t typosquatting-livefeed-backend ./python-backend
 docker build -t typosquatting-livefeed-frontend ./web-frontend
+chmod +700 ./start.sh
