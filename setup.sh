45a8740d666b9836d46046d5da534a55b3531bc4
Kaleb Williams <kw@kalebwilliams.com>
2024-04-13 14:16:25 -0400

    Created docker-compose and setup files

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+pip install -r ./python-backend/requirements.txt
+pip install docker-compose
+docker build -t typosquatting-livefeed-backend ./python-backend
+docker build -t typosquatting-livefeed-frontend ./web-frontend
+docker compose up
