b1f6a496a2b82f60c10e94418491490c7b68621f
Kaleb Williams <kw@kalebwilliams.com>
2024-04-13 14:19:24 -0400

    Set setup command to run detached

--- setup.sh
+++ setup.sh
@@ -4,4 +4,4 @@ pip install -r ./python-backend/requirements.txt
 pip install docker-compose
 docker build -t typosquatting-livefeed-backend ./python-backend
 docker build -t typosquatting-livefeed-frontend ./web-frontend
-docker compose up
+docker compose up -d
