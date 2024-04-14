68bacc2dfde9abf9ef63e2829e3acbfa584bce0e
Kaleb Williams <kw@kalebwilliams.com>
2024-04-14 12:37:00 -0400

    Switched to docker public repo

--- setup.sh
+++ setup.sh
@@ -2,6 +2,5 @@
 
 pip install -r ./python-backend/requirements.txt
 pip install docker-compose
-docker build -t typosquatting-livefeed-backend ./python-backend
-docker build -t typosquatting-livefeed-frontend ./web-frontend
+docker image pull -a gatheringrays/typosquatting-livefeed
 chmod +700 ./start.sh
