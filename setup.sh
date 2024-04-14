59cb50b853ea54a5bb11da32f8897cb029b8aaa9
Kaleb Williams <166882203+kwilliams49221@users.noreply.github.com>
2024-04-14 12:59:06 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pip install -r ./python-backend/requirements.txt
+pip install --upgrade -r ./python-backend/requirements.txt
 pip install docker-compose
 docker image pull -a gatheringrays/typosquatting-livefeed
 chmod +700 ./start.sh
